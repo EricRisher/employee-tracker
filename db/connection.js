d49a7e6b40a8b08cb240dec21e9974d32fab6036
EricRisher <rishereric13@gmail.com>
2023-12-12 19:48:18 -0800

    added comments

--- db/connection.js
+++ db/connection.js
@@ -1,16 +1,21 @@
+// Require the dotenv package to use environment variables
 require('dotenv').config();
+
+// Import necessary modules from Sequelize
 const { Sequelize } = require('sequelize');
 const mysql = require('mysql2');
 
+// Create a new Sequelize instance with database connection details
 const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PW,
+    process.env.DB_NAME,     // Database name from environment variables
+    process.env.DB_USER,     // Database user from environment variables
+    process.env.DB_PW,       // Database password from environment variables
     {
-        host: 'localhost',
-        dialect: 'mysql',
-        port: 3306
+        host: 'localhost',    // Database host
+        dialect: 'mysql',     // Dialect (type) of the database
+        port: 3306             // Database port
     }
 );
 
+// Export the Sequelize instance for use in other parts of the application
 module.exports = sequelize;
--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const inquirer = require("inquirer");
 const db = require("./db/connection");
 
+// Function to view all employees
 const viewAllEmployees = async () => {
   try {
+    // SQL query to retrieve employee details with additional information
     const query = `
       SELECT e.id, e.first_name, e.last_name, e.role_id, e.manager_id,
              r.title AS role_title, r.salary AS role_salary,
@@ -14,10 +16,12 @@ const viewAllEmployees = async () => {
       LEFT JOIN department d ON r.department_id = d.id
     `;
 
+    // Execute the query
     const employees = await db.query(query, {
       type: db.QueryTypes.SELECT,
     });
 
+    // Format the retrieved employees data for better display
     const formattedEmployees = employees.map((employee) => {
       return {
         ID: employee.id,
@@ -30,6 +34,7 @@ const viewAllEmployees = async () => {
       };
     });
 
+    // Display the formatted employee data in a table
     console.table(formattedEmployees);
     start();
   } catch (error) {
@@ -38,10 +43,13 @@ const viewAllEmployees = async () => {
   }
 };
 
+// Function to add a new employee
 const addEmployee = async () => {
   try {
+    // Retrieve roles from the database
     const roles = await db.query("SELECT * FROM role");
 
+    // Prompt the user for employee details
     inquirer
       .prompt([
         {
@@ -83,6 +91,7 @@ const addEmployee = async () => {
       ])
       .then(async (answers) => {
         try {
+          // Insert the new employee into the database
           await db.query(
             "INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)",
             {
@@ -108,8 +117,9 @@ const addEmployee = async () => {
   }
 };
 
-
+// Function to update an employee's role
 const updateEmployeeRole = async () => {
+  // Prompt the user to select an employee and a new role
   inquirer
     .prompt([
       {
@@ -126,7 +136,6 @@ const updateEmployeeRole = async () => {
             };
           });
         },
-
       },
       {
         type: "list",
@@ -146,6 +155,7 @@ const updateEmployeeRole = async () => {
     ])
     .then(async (answers) => {
       try {
+        // Retrieve the employee's last name
         const [employee] = await db.query(
           "SELECT last_name FROM employee WHERE id = ?",
           {
@@ -154,11 +164,13 @@ const updateEmployeeRole = async () => {
           }
         );
 
+        // Check if the employee exists
         if (!employee) {
           console.log("Employee not found");
           return start();
         }
 
+        // Update the employee's role in the database
         await db.query("UPDATE employee SET role_id = ? WHERE id = ?", {
           replacements: [answers.role_id, answers.employee_id],
           type: db.QueryTypes.UPDATE,
@@ -173,16 +185,20 @@ const updateEmployeeRole = async () => {
     });
 };
 
+// Function to view all roles
 const viewAllRoles = async () => {
   try {
+    // SQL query to retrieve all roles with additional information
     const query = `
       SELECT r.id AS ID, r.title AS Title, r.salary AS Salary, d.name AS Department
       FROM role r
       LEFT JOIN department d ON r.department_id = d.id
     `;
 
+    // Execute the query
     const [roles] = await db.query(query);
 
+    // Format the retrieved roles data for better display
     const formattedRoles = roles.map((role) => {
       return {
         ID: role.ID,
@@ -200,11 +216,13 @@ const viewAllRoles = async () => {
   }
 };
 
-
+// Function to add a new role
 const addRole = async () => {
   try {
+    // Retrieve departments from the database
     const departments = await db.query("SELECT * FROM department");
 
+    // Prompt the user for role details
     const answers = await inquirer.prompt([
       {
         type: "input",
@@ -219,7 +237,8 @@ const addRole = async () => {
       {
         type: "list",
         name: "department_id",
-        message: "Which department does this role belong to? (Enter department ID):",
+        message:
+          "Which department does this role belong to? (Enter department ID):",
         choices: departments[0].map((department) => {
           return {
             name: department.name,
@@ -229,6 +248,7 @@ const addRole = async () => {
       },
     ]);
 
+    // Insert the new role into the database
     await db.query(
       "INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)",
       {
@@ -245,8 +265,10 @@ const addRole = async () => {
   }
 };
 
+// Function to view all departments
 const viewAllDepartments = async () => {
   try {
+    // Retrieve all departments from the database
     const [departments, metadata] = await db.query("SELECT * FROM department");
 
     // Remove duplicates based on the 'id' field
@@ -256,6 +278,7 @@ const viewAllDepartments = async () => {
       return departments.find((d) => d.id === id);
     });
 
+    // Display the unique department data in a table
     console.table(uniqueDepartments);
     start();
   } catch (error) {
@@ -264,12 +287,15 @@ const viewAllDepartments = async () => {
   }
 };
 
+// Function to add a new department
 const addDepartment = async () => {
   try {
+    // Retrieve existing departments from the database
     const [existingDepartments, metadata] = await db.query(
       "SELECT * FROM department"
     );
 
+    // Prompt the user for department details
     inquirer
       .prompt([
         {
@@ -285,11 +311,13 @@ const addDepartment = async () => {
             (dep) => dep.name === answers.name
           );
 
+          // If department exists, inform the user and exit
           if (departmentExists) {
             console.log("Department with the same name already exists");
             return start();
           }
 
+          // Insert the new department into the database
           await db.query("INSERT INTO department (name) VALUES (?)", {
             replacements: [answers.name],
             type: db.QueryTypes.INSERT,
@@ -308,6 +336,7 @@ const addDepartment = async () => {
   }
 };
 
+// Function to start the application and prompt user for actions
 const start = () => {
   console.log("Welcome to the Employee Tracker!");
   inquirer
@@ -361,6 +390,17 @@ const start = () => {
     });
 };
 
+// Start the application
 start();
 
-module.exports = { viewAllEmployees, addEmployee, updateEmployeeRole, viewAllRoles, addRole, viewAllDepartments, addDepartment, start };
+// Export functions for testing
+module.exports = {
+  viewAllEmployees,
+  addEmployee,
+  updateEmployeeRole,
+  viewAllRoles,
+  addRole,
+  viewAllDepartments,
+  addDepartment,
+  start,
+};
